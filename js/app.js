334e59d7d66a569ba419637e849af8efdc21683d
Cristiano Freese <cristianofreese@terra.com.br>
2022-10-21 22:41:16 -0300

    9o commit - conclusão rotinas js

--- js/app.js
+++ js/app.js
@@ -1,71 +1,89 @@
 /*DESENVOLVIDO POR CRISTIANO FREESE*/
 /* INJEÇÃO DE HTML NA TAG MAIN DO ARQUIVO INDEX.HTML */
 async function injectContent() {
-    //pega o endereço da url
-    let url = location.href; 
+  let url = location.href; 
+  file = url.split('='); 
+  let view = (file[1] != undefined) ? file[1] : 'inicio'; 
 
-    //divide o endereço pelo = e aplica os valores como array
-    file = url.split('='); 
+  const resp = await fetch(`views/${view}.html`); 
+  const html = await resp.text(); 
 
-    let view = (file[1] != undefined) ? file[1] : 'inicio'; 
-
-    //pega o valor após o = da url e aplica no fetch
-    const resp = await fetch(`views/${view}.html`); 
-    //o método fetch faz uma requisiçao ajax para um determinado local, neste caso para views/arquivo.html
-
-    //converte o resultado da fetch para texto
-    const html = await resp.text(); 
-
-    //seleciona o destino para carregar o arquivo
-    let inject = document.getElementById('content'); 
-
-    inject.innerHTML = html; //aplica a injeção de conteúdo no container de destino
+  let inject = document.getElementById('content'); 
+  inject.innerHTML = html; //aplica a injeção de conteúdo no container de destino
 }
 
 function onclickFooter(){
   document.getElementById("footer-social").style.color= 'red';
 }
 
-// Função para armazenar data, hora e página acessada no Local Storage
-function CarregouPagina(){
-  var data = new Date();
+// FUNÇÃO AUXILIAR PARA ADICIONAR 0 NA STRINGS, QUANDO NUMERO MENOR QUE 10
+function AdicionaZero(nraux){
+  if (nraux <= 9) return `0${nraux}`;
+  else return nraux;
+}
 
-  var dia     = data.getDate();           
-  var mes     = data.getMonth();          
-  var ano    = data.getFullYear();           
-  var hora    = data.getHours();          
-  var min     = data.getMinutes();        
-  var seg     = data.getSeconds();        
-
-  var dataVis = `${dia}/${mes+1}/${ano}`;
-  var horaVis = `${hora}:${min}:${seg}`;
+//FUNÇÃO PARA ARMAZENAR DATA, HORA E PÁGINA ACESSADA NO LOCAL STORAGE
+function CarregouPagina(posicao){
+  let data = new Date();
+  let dia     = data.getDate();           
+  let mes     = data.getMonth();          
+  let ano    = data.getFullYear();           
+  let hora    = data.getHours();          
+  let min     = data.getMinutes();        
+  let seg     = data.getSeconds();        
+  let dataVis = `${AdicionaZero(dia)}/${AdicionaZero(mes+1)}/${AdicionaZero(ano)}`;
+  let horaVis = `${AdicionaZero(hora)}:${AdicionaZero(min)}:${AdicionaZero(seg)}`;
 
   let url = location.href;
   let view = url.split('=');
-  let paginaVis = (view[1] != undefined) ? view[1] : 'inicio'; 
-  console.log(`${dataVis} - ${horaVis} - ${paginaVis}`);
-    let storage = (localStorage.VISITAS) ? JSON.parse(localStorage.VISITAS) : [];
+  let paginaVis = (view[1] != undefined) ? view[1] : 'Inicio';
+  paginaVis = paginaVis.charAt(0).toUpperCase() + paginaVis.slice(1); 
+  let storage = (localStorage.VISITAS) ? JSON.parse(localStorage.VISITAS) : [];
 
-    // definir objeto do veículo
-    let visita = {
-      "DataVis" : dataVis,
-      "HoraVis" : horaVis,
-      "PaginaVis" : paginaVis
-    }
+  let visita = {
+  "DataVis" : dataVis,
+  "HoraVis" : horaVis,
+  "PaginaVis" : paginaVis,
+  "Coordenadas" : 'LAT, LON'
+  }
 
-    storage.push(visita);
-    
-    // salvar no localStorage
-    localStorage.setItem('VISITAS', JSON.stringify(storage));
+  storage.push(visita);
+  localStorage.setItem('VISITAS', JSON.stringify(storage));
 }
 
+// FUNÇÃO PARA ARMAZENAR CLIQUE NO ACCORDION LEI ROUANET NO LOCAL STORAGE 
 function onclickAccordion(){
-  console.log("Clicou no Accordion");
+  let data = new Date();
+  let dia     = data.getDate();           
+  let mes     = data.getMonth();          
+  let ano    = data.getFullYear();           
+  let dataClique = `${AdicionaZero(dia)}/${AdicionaZero(mes+1)}/${AdicionaZero(ano)}`;
+  
+  let storage = (localStorage.CLIQUES) ? JSON.parse(localStorage.CLIQUES) : [];
+
+  let cliques = {
+  "DataClique" : dataClique,
+  "TipoClique" : 'LEI ROUANET'
+  }
+
+  storage.push(cliques);
+  localStorage.setItem('CLIQUES', JSON.stringify(storage));
+}
+
+// FUNÇÃO SUCCESS - COORDENADAS GEOGRAFICAS - GETCURRENTPOSITION
+function success(posicao){
+  console.log(`Latitude : ${posicao.coords.latitude}, ${posicao.coords.longitude}`);
+}
+
+// FUNÇÃO ERROR - COORDENADAS GEOGRAFICAS - GETCURRENTPOSITION
+function error(err){
+  console.log(err);
 }
 
 /* EXECUÇÃO DAS FUNÇÕES */
 injectContent();
-
+//CONSULTA LOCALIZAÇÃO USUÁRIO VIA API GEOLOCATION
+navigator.geolocation.getCurrentPosition(success, error);
 
 /********************* FEITO POR SANDRA *************/
 
